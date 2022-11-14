47eddeff3d03198e5b4df9b4f2e208063b3c1de1
chrg <christian.ricci2000@gmail.com>
2022-11-14 23:26:00 +0100

    Finish report for real

--- test/make_graph.py
+++ test/make_graph.py
@@ -7,9 +7,9 @@ ws           = [ 240, 480, 960, 1920, 3840, 7680 ]
 hs           = [ 135, 270, 540, 1080, 2160, 4320 ]
 
 serial          = [0.157987, 0.629014, 2.515071, 10.093090, 40.648059, 165.002237, 36.5075]
-parallel        = [0.001217, 0.002516, 0.009254, 0.042987,  0.164412,  0.602978,   0.1372]
 serial_ghost    = [0.026404, 0.112614, 0.432801, 1.685960,  8.995003,  35.104033,  7.7261]
-parallel_ghost  = [0.001065, 0.007141, 0.002282, 0.032478,  0.156047,  0.465563,   0.08501483]
+parallel        = [0.001214, 0.002641, 0.009275, 0.043020,  0.169831,  0.607439,   0.1389]
+parallel_ghost  = [0.000889, 0.002288, 0.007144, 0.028267,  0.177704,  0.493579,   0.1183]
 
 def throughput(t, w, h):
     return w * h * (4 + 4 + 30 + 30*2 + 1 + 32 + 1) / t
@@ -40,7 +40,7 @@ def graph_sp(program, data):
     plt.savefig(program)
 
 graph("Seriale", serial)
-graph("Serial (Ghost Area)", serial_ghost)
+graph("Seriale (Ghost Area)", serial_ghost)
 graph("Parallelo", parallel)
 graph("Parallelo (Ghost Area)", parallel_ghost)
 
